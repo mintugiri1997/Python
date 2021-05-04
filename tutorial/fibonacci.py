8e17c5f04ab21d7c8a964e38125532f3dbe409cf
mintu <mintu.giri1997@gmail.com>
2021-05-05 01:41:36 +0530

    added fibonacci series program

---
+++ tutorial/fibonacci.py
@@ -0,0 +1,13 @@
+num1 = 0
+num2 = 1
+count = 20
+
+series = f"{num1} {num2}"
+
+for i in range(count):
+    num3 = num1 + num2
+    num1 = num2
+    num2 = num3
+    series += f' {num3}'
+
+print(series)
