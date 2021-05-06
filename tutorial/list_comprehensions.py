3c6b41abd977969d934c22320eff6b6019ebfbf2
mintu <mintu.giri1997@gmail.com>
2021-05-06 21:22:48 +0530

    list_comprehension added

---
+++ tutorial/list_comprehensions.py
@@ -0,0 +1,30 @@
+#List Comprehension
+
+#Normal Method
+
+square_list = []
+for item in range(1,11):
+    square_list.append(item**2)
+print(square_list)
+
+#Comprehension Method
+
+square_list_comprehension = [item**2 for item in range(1,11)]
+print(square_list_comprehension)
+
+#Comprehension with  if condition
+
+square_list_comprehension_condition = [item for item in range(1,11) if item%2 ==0]
+print(square_list_comprehension_condition)
+
+#Comprehension with  if else condition
+
+new_list = [item**2 if (item%2 == 0) else -item for item in range(1,11)]
+print(new_list)
+
+#Comprehension with nested list
+
+nested_list = [[i for i in range(1,4)] for j in range(3)]
+print(nested_list)
+
+
